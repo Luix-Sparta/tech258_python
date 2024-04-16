54bd5de7bf1707b2d29d5eea46742e3992216f11
Luix-Sparta <luixhiano@gmail.com>
2024-04-16 11:42:31 +0100

    Updated json_2_yaml.py

--- scripting/json/json_2_yaml.py
+++ scripting/json/json_2_yaml.py
@@ -1,22 +1,21 @@
 import sys, os, json, yaml
 
-
-if len(sys.argv) > 1: # do we have more than 1 argument?
-    if os.path.exists(sys.argv[1]): # is the filename passed actually present?
+if len(sys.argv) > 1:  # do we have more than 1 argument?
+    if os.path.exists(sys.argv[1]):  # is the filename passed actually present?
         # Convert Json to dict
-        json_string = json.loads(open(sys.argv[1]).read())
+        json_data = json.load(open(sys.argv[1]))
 
         # Convert dict to yaml
-        yaml_string = yaml.dump(json_string)
+        yaml_data = yaml.dump(json_data)
 
         # Check if the argument ends with .yaml
-        if (sys.argv[2].endswith(".yaml")):
+        if sys.argv[2].endswith(".yaml"):
 
             # Checking if Yaml exists
             if not os.path.exists(sys.argv[2]):
-            # Then create yaml and input yaml_string
+                # Then create yaml and input yaml_string
                 with open(sys.argv[2], "w") as yaml_file:
-                    yaml_file.write(yaml_string)
+                    yaml_file.write(yaml_data)
                     print(f"Yaml file created: {sys.argv[2]}")
             else:
                 # Checking if .yaml file exists
@@ -29,4 +28,3 @@ if len(sys.argv) > 1: # do we have more than 1 argument?
 
 else:
     print("Usage: python json_2_yaml.py <file.json> <file.yaml>")
-
